caa3e5475ef36c0fc86054f990f325306166f581
Ori <152011726+Nx0Ri@users.noreply.github.com>
2026-08-14 02:57:58 +0700

    Update MainActivity.kt?

--- app/src/main/java/dev/busung/s25uroot/MainActivity.kt
+++ app/src/main/java/dev/busung/s25uroot/MainActivity.kt
@@ -260,7 +260,7 @@ private val languageOptions = listOf(
 )
 
 private const val KERNEL_SU_MANAGER_URL =
-    "https://github.com/backslashxx/KernelSU/releases/download/v3.2.5-59/KernelSU_v3.2.5-59_32584-release.apk"
+    "https://github.com/backslashxx/KernelSU/releases/download/v3.2.5-59%2B2/KernelSU_v3.2.5-59+2_32586-release.apk"
 private const val KERNEL_SU_MANAGER_PACKAGE = "me.weishu.kernelsu"
 private const val KERNEL_SU_HOME_URL = "https://kernelsu.org/"
 private const val SHIZUKU_MANAGER_PACKAGE = "moe.shizuku.manager"
